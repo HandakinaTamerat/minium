b73a1f3d5eb10e13fae2a6562ec29be71a209706
handakina <han7jello@gmail.com>
2019-09-22 17:05:26 -0500

    registration service created

--- client/MiniumClient/src/app/app.module.ts
+++ client/MiniumClient/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { MaterialModule } from './sharedmodules/material.module';
 import { AuthModule } from './services/auth/auth.module';
 import { RouterModule } from '@angular/router';
 import { LandingpageComponent } from './landingpage/landingpage.component';
-import { UsersModule } from './users/users.module';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
