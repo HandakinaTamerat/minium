2460d6b96b81fc2b9590313fbe4be08f53398ae2
handakina <han7jello@gmail.com>
2019-09-22 11:02:51 -0500

    login form done

--- client/MiniumClient/src/app/users/login/login.component.ts
+++ client/MiniumClient/src/app/users/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +9,21 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   form;
-  constructor(private builder:FormBuilder) { }
+  constructor(private builder:FormBuilder,private route:Router,private authService:AuthService) { }
 
   ngOnInit() {
     this.form=this.builder.group({
-      'email':['',Validators.required,Validators.email],
-      'password':['',Validators.required,Validators.min(6)]
+      'email':['',Validators.compose([Validators.required,Validators.email])],
+      'password':['',Validators.compose([Validators.required,Validators.minLength(6)])]
     });
+  }
+
+  login(){
+    this.authService.login(this.form.value);
+  }
+
+  redirectToRigester(){
+    this.route.navigate(['/users/signup']);
   }
 
 }
