63b9106a0cd13ea6f22506e961dfd881340b9981
biniam tenaw <biniam.xh@gmail.com>
2019-09-22 16:57:37 -0500

    create models, create post components

--- client/MiniumClient/src/app/posts/posts.module.ts
+++ client/MiniumClient/src/app/posts/posts.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewPostComponent } from './new-post/new-post.component';
+import { SinglePostComponent } from './single-post/single-post.component';
+import { PostListingComponent } from './post-listing/post-listing.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NewPostComponent, SinglePostComponent, PostListingComponent],
   imports: [
     CommonModule
   ]
