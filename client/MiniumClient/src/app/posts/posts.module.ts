ca4b8b615f4db9c277d76cd6750a8d0788194da3
biniam tenaw <biniam.xh@gmail.com>
2019-09-22 17:44:50 -0500

    create minimal post service http request methods

--- client/MiniumClient/src/app/posts/posts.module.ts
+++ client/MiniumClient/src/app/posts/posts.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { NewPostComponent } from './new-post/new-post.component';
 import { SinglePostComponent } from './single-post/single-post.component';
 import { PostListingComponent } from './post-listing/post-listing.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  {path:'post', component: NewPostComponent},
+  {path:':/postID', component: SinglePostComponent},
+  {path:'', component: PostListingComponent}
+]
 
 
 @NgModule({
   declarations: [NewPostComponent, SinglePostComponent, PostListingComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class PostsModule { }
--- client/MiniumClient/src/app/posts/posts.service.ts
+++ client/MiniumClient/src/app/posts/posts.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Post } from '../sharedmodules/posts.models';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+ 
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  getPostsUrl = `${environment.apiUrl}/posts`
+  newPostUrl = `${environment.apiUrl}/post`
+  getSinglePostUrl = `${environment.apiUrl}/:postId`
+  getUserPostsUrl = `${environment.apiUrl}/userId`
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  getPosts():Observable<Post[]>{
+    return this.http.get<Post[]>(this.getPostsUrl)
+  }
+
+  getUserPosts():Observable<Post[]>{
+    return this.http.get<Post[]>(this.getPostsUrl)
+  }
+
+  newPost(body: Post):Observable<any>{
+    //const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post(this.newPostUrl, body)
+  }
+
+
 }
