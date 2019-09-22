3e2c3126eb2ec8c418b24f9c0c626e029fea6ade
iaidar <iaidar@mum.edu>
2019-09-22 13:31:54 -0500

    fix conflict

--- client/MiniumClient/src/app/users/login/login.component.ts
+++ client/MiniumClient/src/app/users/login/login.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-<<<<<<< HEAD
 import { LoginService } from 'src/app/services/auth/login.service';
-=======
 import { Router } from '@angular/router';
->>>>>>> 17911a85646111da859e9531d7327d920c8551fe
+
 
 @Component({
   selector: 'app-login',
@@ -12,13 +10,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-<<<<<<< HEAD
   public form;
-  constructor(private builder:FormBuilder, private loginService: LoginService) { }
-=======
-  form;
-  constructor(private builder:FormBuilder,private route:Router,private authService:AuthService) { }
->>>>>>> 17911a85646111da859e9531d7327d920c8551fe
+  constructor(private builder:FormBuilder,private route:Router,private loginService:LoginService) { }
 
   ngOnInit() {
     this.form=this.builder.group({
@@ -27,18 +20,13 @@ export class LoginComponent implements OnInit {
     });
   }
 
-<<<<<<< HEAD
   async login() {
     console.log(this.form)
     // await this.loginService.login(this.form)
-=======
-  login(){
-    this.authService.login(this.form.value);
   }
 
   redirectToRigester(){
     this.route.navigate(['/users/signup']);
->>>>>>> 17911a85646111da859e9531d7327d920c8551fe
   }
 
 }
