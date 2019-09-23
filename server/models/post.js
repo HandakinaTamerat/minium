b293f4872c6d2ecf39dbab23b61f560f4a899f67
iaidar <iaidar@mum.edu>
2019-09-23 15:07:33 -0500

    prepare to push to heroku

--- server/models/post.js
+++ server/models/post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const postSchema = new Schema({
     title: {type: String, required: [true, "can't be blank"]},
-    description: String,
+    content: String,
     user: { 
         type: Schema.Types.ObjectId, 
         ref: 'users' 
