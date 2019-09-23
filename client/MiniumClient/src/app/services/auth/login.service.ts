e610601737e5dc4bcd73d93da64c5e6544f2706f
handakina <han7jello@gmail.com>
2019-09-23 15:03:02 -0500

    commit interceptor

--- client/MiniumClient/src/app/services/auth/login.service.ts
+++ client/MiniumClient/src/app/services/auth/login.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
+import { enviroment as env} from '../../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
   subscription;
+  url=env.authApiUrl;
   constructor(private http:HttpClient,private auth:AuthService) { }
 
   login(user){
-    return this.http.post('/api/login',user);
+    return this.http.post(`${this.url}/login`,user);
   }
 
   storeToken(obj){
--- client/MiniumClient/src/app/services/auth/register.service.ts
+++ client/MiniumClient/src/app/services/auth/register.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from 'src/app/sharedmodules/user.models';
 import { AuthService } from './auth.service';
+import { environment as env }  from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  url=env.authApiUrl;
   constructor(private http:HttpClient,private authService:AuthService) {  }
 
   register(user:User){
-    return this.http.post('/api/register',user);
+    return this.http.post(`${this.url}/register`,user);
   }
 
   storeToken(data){
